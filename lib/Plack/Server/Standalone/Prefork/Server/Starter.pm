8f2ab6d2f6a2447a0fcfdc176d0f9f398c32c8c4
Kazuho Oku <kazuhooku@gmail.com>
2009-12-09 13:14:56 +0900

    first commit

---
+++ lib/Plack/Server/Standalone/Prefork/Server/Starter.pm
@@ -0,0 +1,59 @@
+package Plack::Server::Standalone::Prefork::Server::Starter;
+
+use strict;
+use warnings;
+
+use base qw(Plack::Server::Standalone::Prefork);
+
+sub new {
+    my ($klass, %args) = @_;
+    
+    my ($hostport, $fd) = %{Server::Starter::server_ports()};
+    if ($hostport =~ /(.*):(\d+)/) {
+        $args{host} = $1;
+        $args{port} = $2;
+    } else {
+        $args{port} = $hostport;
+    }
+    $args{listen_sock} = IO::Socket::INET->new(
+        Proto => 'tcp',
+    ) or die "failed to create socket:$!";
+    $args{listen_sock}->fdopen($fd, 'w')
+        or die "failed to bind to listening socket:$!";
+    
+    $klass->SUPER::new(%args);
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Server::Standalone::Prefork::Server::Starter
+
+=head1 SYNOPSIS
+
+  % start_server --port=80 plackup -s Standalone::Prefork::Server::Starter
+
+=head1 DESCRIPTION
+
+Plack::Server::Standalone::Prefork::Server::Starter is a wrapper to manage L<PLack::Server::Standalone::Prefork> using L<Server::Starter>.
+
+All parameters of L<Plack::Server::Standalone::Prefork> except "host" and "port" can be passed to this module.
+
+=head1 AUTHOR
+
+Kazuho Oku
+
+=head1 SEE ALSO
+
+L<Plack::Server::Standalone::Prefork>
+L<Server::Starter>
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+See L<http://www.perl.com/perl/misc/Artistic.html>
+
+=cut
---
+++ t/00base.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+
+use Test::More tests => 1;
+
+BEGIN {
+    use_ok('Server::Starter');
+};
