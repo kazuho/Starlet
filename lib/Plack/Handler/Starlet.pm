664ed682e4da0b4199d5f14404954c193af3d1ea
Kazuho Oku <kazuhooku@gmail.com>
2010-03-26 10:55:25 +0900

    follow changes in plack

--- lib/Plack/Handler/Starlet.pm
+++ lib/Plack/Handler/Starlet.pm
@@ -3,31 +3,55 @@ package Plack::Handler::Starlet;
 use strict;
 use warnings;
 
+use Parallel::Prefork;
 use Server::Starter ();
 use base qw(HTTP::Server::PSGI);
 
 sub new {
     my ($klass, %args) = @_;
     
-    my ($hostport, $fd) = %{Server::Starter::server_ports()};
-    if ($hostport =~ /(.*):(\d+)/) {
-        $args{host} = $1;
-        $args{port} = $2;
-    } else {
-        $args{port} = $hostport;
+    my $listen_sock;
+    if (defined $ENV{SERVER_STARTER_PORT}) {
+        my ($hostport, $fd) = %{Server::Starter::server_ports()};
+        if ($hostport =~ /(.*):(\d+)/) {
+            $args{host} = $1;
+            $args{port} = $2;
+        } else {
+            $args{port} = $hostport;
+        }
+        $listen_sock = IO::Socket::INET->new(
+            Proto => 'tcp',
+        ) or die "failed to create socket:$!";
+        $listen_sock->fdopen($fd, 'w')
+            or die "failed to bind to listening socket:$!";
     }
     
-    $args{max_workers} ||= 10;
-    
     my $self = $klass->SUPER::new(%args);
+    $self->{is_multiprocess} = 1;
+    $self->{max_workers} = $args{max_workers} || 10;
     
-    $self->{listen_sock} = IO::Socket::INET->new(
-        Proto => 'tcp',
-    ) or die "failed to create socket:$!";
-    $self->{listen_sock}->fdopen($fd, 'w')
-        or die "failed to bind to listening socket:$!";
+    $self->{listen_sock} = $listen_sock
+        if $listen_sock;
     
     $self;
+}
+
+sub run {
+    my($self, $app) = @_;
+    $self->setup_listener();
+    my $pm = Parallel::Prefork->new({
+        max_workers => $self->{max_workers},
+        trap_signals => {
+            TERM => 'TERM',
+            HUP => 'TERM',
+        },
+    });
+    while ($pm->signal_received ne 'TERM') {
+        $pm->start and next;
+        $self->accept_loop($app, $self->{max_reqs_per_child});
+        $pm->finish;
+    }
+    $pm->wait_all_children;
 }
 
 1;
